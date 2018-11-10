aba7122608864a586add689425ffa8f67a6acfbd
peter <peter@peters-Mac-mini.local>
2018-11-10 12:39:47 -0800

    ios-tumbler-feed app

---
+++ ios-tumbler-feed/PhotoCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.swift
+//  ios-tumbler-feed
+//
+//  Created by peter on 11/10/18.
+//  Copyright © 2018 petecit. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
